2c0ef58460e8664bfaa0cf85c82a772152f6930d
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-03 01:44:07 +0400

    Create DatabaseEngine.cs

---
+++ src/ROFSDB/DatabaseEngine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace TIKSN.ROFSDB
+{
+    public class DatabaseEngine : IDatabaseEngine
+    {
+        public string[] GetCollectionsAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IAsyncEnumerable<T> GetDocumentsAsync<T>(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
