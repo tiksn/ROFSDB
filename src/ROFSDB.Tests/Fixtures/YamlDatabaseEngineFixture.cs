4a8d74bbe8cb3eb030e6ccff718549fa567532a6
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 13:31:04 +0400

    Create YamlDatabaseEngineFixture.cs

---
+++ src/ROFSDB.Tests/Fixtures/YamlDatabaseEngineFixture.cs
@@ -0,0 +1,31 @@
+﻿using Storage.Net;
+using Storage.Net.Blobs;
+using System.Threading.Tasks;
+using TIKSN.ROFSDB.FileStorageAdapters;
+using Xunit;
+
+namespace TIKSN.ROFSDB.Tests.Fixtures
+{
+    public class YamlDatabaseEngineFixture : IAsyncLifetime
+    {
+        public IDatabaseEngine DatabaseEngine { get; private set; }
+
+        public Task DisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public async Task InitializeAsync()
+        {
+            IBlobStorage blobStorage = await CreateBlobStorageAsync();
+            DatabaseEngine = new DatabaseEngine(new BlobStorageToFileStorageAdapter(blobStorage));
+        }
+
+        private async Task<IBlobStorage> CreateBlobStorageAsync()
+        {
+            var blobStorage = StorageFactory.Blobs.InMemory();
+
+            return blobStorage;
+        }
+    }
+}
