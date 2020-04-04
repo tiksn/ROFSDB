94297749d2da426167e81d2a05577b81b6905bba
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 15:50:49 +0400

    Update IFileStorage.cs

--- src/ROFSDB/IFileStorage.cs
+++ src/ROFSDB/IFileStorage.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TIKSN.ROFSDB
 {
     public interface IFileStorage
     {
+        IAsyncEnumerable<FileStorageEntry> ListAsync(string fullPath, CancellationToken cancellationToken);
+
+        Task<Stream> OpenReadAsync(string fullPath, CancellationToken cancellationToken);
     }
 }
