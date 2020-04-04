2d7d00df07905b3d50531cbde384cc42c8f8fef2
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 17:06:36 +0400

    Create YamlSerialization.cs

---
+++ src/ROFSDB/Serialization/YamlSerialization.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using YamlDotNet.Core;
+using YamlDotNet.Core.Events;
+using YamlDotNet.Serialization;
+
+namespace TIKSN.ROFSDB.Serialization
+{
+    public class YamlSerialization : ISerialization
+    {
+        private static readonly IEnumerable<string> fileExtensions = new[] { ".yml", ".yaml" };
+
+        public IEnumerable<string> FileExtensions => fileExtensions;
+
+        public async IAsyncEnumerable<T> GetDocumentsAsync<T>(Stream stream, CancellationToken cancellationToken)
+        {
+            var streamReader = new StreamReader(stream);
+            var scanner = new Scanner(streamReader);
+            var deserializer = new DeserializerBuilder().Build();
+
+            var parser = new Parser(scanner);
+
+            parser.Consume<StreamStart>();
+
+            while (parser.Accept(out DocumentStart documentStart))
+            {
+                var doc = deserializer.Deserialize<T>(parser);
+
+                yield return doc;
+            }
+        }
+    }
+}
