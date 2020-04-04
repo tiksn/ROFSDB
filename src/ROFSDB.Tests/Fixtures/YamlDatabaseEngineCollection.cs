d6ea38aacbfe98c2ad0921aaf3d7dce683306c0a
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 13:31:08 +0400

    Create YamlDatabaseEngineCollection.cs

---
+++ src/ROFSDB.Tests/Fixtures/YamlDatabaseEngineCollection.cs
@@ -0,0 +1,9 @@
+﻿using Xunit;
+
+namespace TIKSN.ROFSDB.Tests.Fixtures
+{
+    [CollectionDefinition("Yaml Database Engine collection")]
+    public class YamlDatabaseEngineCollection : ICollectionFixture<YamlDatabaseEngineFixture>
+    {
+    }
+}
