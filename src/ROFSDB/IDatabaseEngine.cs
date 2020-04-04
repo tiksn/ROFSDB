21517e6cdecc014fbb3b96092bcb6639809c9361
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 16:01:00 +0400

    Update IDatabaseEngine.cs

--- src/ROFSDB/IDatabaseEngine.cs
+++ src/ROFSDB/IDatabaseEngine.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections.Generic;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace TIKSN.ROFSDB
 {
     public interface IDatabaseEngine
     {
-        Task<string[]> GetCollectionsAsync(CancellationToken cancellationToken);
+        IAsyncEnumerable<string> GetCollectionsAsync(CancellationToken cancellationToken);
 
         IAsyncEnumerable<T> GetDocumentsAsync<T>(string collectionName, CancellationToken cancellationToken);
     }
