46dae2a5f737b4a3ecf91918a564105916655017
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 17:06:18 +0400

    Create ISerialization.cs

---
+++ src/ROFSDB/Serialization/ISerialization.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace TIKSN.ROFSDB.Serialization
+{
+    public interface ISerialization
+    {
+        IEnumerable<string> FileExtensions { get; }
+
+        IAsyncEnumerable<T> GetDocumentsAsync<T>(Stream stream, CancellationToken cancellationToken);
+    }
+}
