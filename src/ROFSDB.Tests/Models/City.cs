d8bc04a7303b6c54d4fe3568826c513b44b955f4
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 13:34:00 +0400

    Create City.cs

---
+++ src/ROFSDB.Tests/Models/City.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TIKSN.ROFSDB.Tests.Models
+{
+    public class City
+    {
+        public Guid CountryID { get; set; }
+
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
