facc4327a3eaaeb22789ec29ac515f633f91a291
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 15:36:04 +0400

    Create FileProviderToFileStorageAdapter.cs

---
+++ src/ROFSDB/FileStorageAdapters/FileProviderToFileStorageAdapter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TIKSN.ROFSDB.FileStorageAdapters
+{
+    public class FileProviderToFileStorageAdapter : IFileStorage
+    {
+        private readonly IFileProvider fileProvider;
+
+        public FileProviderToFileStorageAdapter(IFileProvider fileProvider)
+        {
+            this.fileProvider = fileProvider ?? throw new ArgumentNullException(nameof(fileProvider));
+        }
+    }
+}
