f579d75f86ff20f37727fe1edd5bd671b6d21e71
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 12:37:33 +0400

    Create DatabaseEngineTests.cs

---
+++ src/ROFSDB.Tests/DatabaseEngineTests.cs
@@ -0,0 +1,26 @@
+﻿using Storage.Net;
+using Storage.Net.Blobs;
+using System;
+using Xunit.Abstractions;
+
+namespace TIKSN.ROFSDB.Tests
+{
+    public class DatabaseEngineTests : IDisposable
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+        private readonly IDatabaseEngine databaseEngine;
+        private readonly IBlobStorage blobStorage;
+
+        public DatabaseEngineTests(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper ?? throw new ArgumentNullException(nameof(testOutputHelper));
+            this.blobStorage = StorageFactory.Blobs.InMemory();
+            this.databaseEngine = new DatabaseEngine();
+        }
+
+        public void Dispose()
+        {
+            blobStorage.Dispose();
+        }
+    }
+}
