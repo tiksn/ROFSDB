e6ef1aee0a5c4cb0dc5171092e445e31b8001eb1
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 17:06:42 +0400

    Update DatabaseEngine.cs

--- src/ROFSDB/DatabaseEngine.cs
+++ src/ROFSDB/DatabaseEngine.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
+using TIKSN.ROFSDB.Serialization;
 
 namespace TIKSN.ROFSDB
 {
     public class DatabaseEngine : IDatabaseEngine
     {
         private readonly IFileStorage fileStorage;
+        private readonly ISerialization serialization;
 
-        public DatabaseEngine(IFileStorage fileStorage)
+        public DatabaseEngine(IFileStorage fileStorage, ISerialization serialization)
         {
             this.fileStorage = fileStorage ?? throw new ArgumentNullException(nameof(fileStorage));
+            this.serialization = serialization ?? throw new ArgumentNullException(nameof(serialization));
         }
 
         public IAsyncEnumerable<string> GetCollectionsAsync(CancellationToken cancellationToken)
@@ -22,9 +25,20 @@ namespace TIKSN.ROFSDB
                 .Select(x => x.Name);
         }
 
-        public IAsyncEnumerable<T> GetDocumentsAsync<T>(string collectionName, CancellationToken cancellationToken)
+        public async IAsyncEnumerable<T> GetDocumentsAsync<T>(string collectionName, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            var files = fileStorage
+                .ListAsync($"/{collectionName}", cancellationToken)
+                .Where(x => !x.IsDirectory && serialization.FileExtensions.Any(e => x.Name.EndsWith(e, StringComparison.OrdinalIgnoreCase)));
+
+            await foreach (var file in files)
+            {
+                using var stream = await fileStorage.OpenReadAsync($"/{collectionName}/{file.Name}", cancellationToken);
+                await foreach (var doc in serialization.GetDocumentsAsync<T>(stream, cancellationToken))
+                {
+                    yield return doc;
+                }
+            }
         }
     }
 }
