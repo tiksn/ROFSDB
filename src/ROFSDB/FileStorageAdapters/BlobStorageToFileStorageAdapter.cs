4a3815fdc9ae54aaf5a1b610ae437a7abf987533
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 15:51:04 +0400

    Update BlobStorageToFileStorageAdapter.cs

--- src/ROFSDB/FileStorageAdapters/BlobStorageToFileStorageAdapter.cs
+++ src/ROFSDB/FileStorageAdapters/BlobStorageToFileStorageAdapter.cs
@@ -1,5 +1,9 @@
 ﻿using Storage.Net.Blobs;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace TIKSN.ROFSDB.FileStorageAdapters
 {
@@ -10,6 +14,24 @@ namespace TIKSN.ROFSDB.FileStorageAdapters
         public BlobStorageToFileStorageAdapter(IBlobStorage blobStorage)
         {
             this.blobStorage = blobStorage ?? throw new ArgumentNullException(nameof(blobStorage));
+        }
+
+        public async IAsyncEnumerable<FileStorageEntry> ListAsync(string fullPath, CancellationToken cancellationToken)
+        {
+            var blobs = await blobStorage.ListAsync(new ListOptions
+            {
+                FolderPath = fullPath
+            }, cancellationToken);
+
+            foreach (var blob in blobs)
+            {
+                yield return new FileStorageEntry(blob.IsFolder, blob.Name);
+            }
+        }
+
+        public Task<Stream> OpenReadAsync(string fullPath, CancellationToken cancellationToken)
+        {
+            return blobStorage.OpenReadAsync(fullPath);
         }
     }
 }
