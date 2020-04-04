c3219c5ffe9f6aae1586b4ad2f4663a353b51b83
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 13:33:53 +0400

    Create Country.cs

---
+++ src/ROFSDB.Tests/Models/Country.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TIKSN.ROFSDB.Tests.Models
+{
+    public class Country
+    {
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
