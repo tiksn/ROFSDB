e4226f31048b7338215dcba4264eb35ee334afd9
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 14:00:29 +0400

    Update City.cs

--- src/ROFSDB.Tests/Models/City.cs
+++ src/ROFSDB.Tests/Models/City.cs
@@ -1,12 +1,10 @@
-﻿using System;
-
-namespace TIKSN.ROFSDB.Tests.Models
+﻿namespace TIKSN.ROFSDB.Tests.Models
 {
     public class City
     {
-        public Guid CountryID { get; set; }
+        public int CountryID { get; set; }
 
-        public Guid ID { get; set; }
+        public int ID { get; set; }
 
         public string Name { get; set; }
     }
