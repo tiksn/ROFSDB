f7e1a59b104bd50d95a22fbb01d3ba31f76602f2
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 16:00:55 +0400

    Update FileStorageEntry.cs

--- src/ROFSDB/FileStorageEntry.cs
+++ src/ROFSDB/FileStorageEntry.cs
@@ -1,9 +1,16 @@
-﻿namespace TIKSN.ROFSDB
+﻿using System;
+
+namespace TIKSN.ROFSDB
 {
     public class FileStorageEntry
     {
         public FileStorageEntry(bool isDirectory, string name)
         {
+            IsDirectory = isDirectory;
+            Name = name ?? throw new ArgumentNullException(nameof(name));
         }
+
+        public bool IsDirectory { get; }
+        public string Name { get; }
     }
 }
