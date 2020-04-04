428a1580da250db62a76e91801bc30b46aa13ef1
Tigran TIKSN Torosyan <personal@tiksn.com>
2020-04-04 13:30:59 +0400

    Create BlobStorageToFileStorageAdapter.cs

---
+++ src/ROFSDB/FileStorageAdapters/BlobStorageToFileStorageAdapter.cs
@@ -0,0 +1,15 @@
+﻿using Storage.Net.Blobs;
+using System;
+
+namespace TIKSN.ROFSDB.FileStorageAdapters
+{
+    public class BlobStorageToFileStorageAdapter : IFileStorage
+    {
+        private readonly IBlobStorage blobStorage;
+
+        public BlobStorageToFileStorageAdapter(IBlobStorage blobStorage)
+        {
+            this.blobStorage = blobStorage ?? throw new ArgumentNullException(nameof(blobStorage));
+        }
+    }
+}
