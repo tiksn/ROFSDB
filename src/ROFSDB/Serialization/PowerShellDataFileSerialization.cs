483e43a44d02155e84c3069f6abb5db6b267aedc
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-11-17 18:15:45 -0600

    Add PowerShell Data File Serialization Support
    - Introduced a new serialization class for PowerShell Data files (.psd1).
    - Updated ServiceCollectionExtensions to register the new serialization.
    - Added System.Management.Automation package reference for parsing PowerShell data.

---
+++ src/ROFSDB/Serialization/PowerShellDataFileSerialization.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Management.Automation.Language;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Threading;
+
+namespace TIKSN.ROFSDB.Serialization;
+
+public class PowerShellDataFileSerialization : ISerialization
+{
+    private static readonly IEnumerable<string> fileExtensions = [".psd1"];
+    public IEnumerable<string> FileExtensions => fileExtensions;
+
+    public async IAsyncEnumerable<T> GetDocumentsAsync<T>(
+        Stream stream,
+        [EnumeratorCancellation] CancellationToken cancellationToken)
+        where T : class, new()
+    {
+        using var streamReader = new StreamReader(stream);
+        var content = await streamReader.ReadToEndAsync(cancellationToken);
+        var psd1Data = ParsePowerShellData(content);
+        var json = JsonSerializer.Serialize(psd1Data);
+        var model = JsonSerializer.Deserialize<T>(json);
+        if (model != null)
+        {
+            yield return model;
+        }
+    }
+
+    private static object ParsePowerShellData(string content)
+    {
+        var ast = Parser.ParseInput(content, out var _, out var errors);
+        if (errors.Length > 0)
+        {
+            throw new FormatException("Could not parse as a PowerShellData file format");
+        }
+        else
+        {
+            var data = ast.Find(static a => a is HashtableAst, false);
+            if (data != null)
+            {
+                return data.SafeGetValue();
+            }
+            else
+            {
+                throw new FormatException("Could not parse as a PowerShellData file format, No Hashtable found");
+            }
+        }
+    }
+}
